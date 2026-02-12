ce65a44ad1289bc404779900fba33315335393b4
Thomas Guan <thomas@NASA-PC.localdomain>
2026-02-12 17:55:54 +1100

    added cards to dashboard page

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,19 @@
 import { Navbar } from "../components/Navbar";
-import { Box, Typography, useMediaQuery } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import GridViewIcon from '@mui/icons-material/GridView';
+
+const games = ["Memory Dictionary", "Sliding Tiles"];
+const links = ["/game/memorydictionary", "/game/slidingtiles"];
+const descriptions = [];
+const icons = [<AutoStoriesIcon />, ]
 
 export function Dashboard() {
   const isSmall = useMediaQuery("(max-width: 500px)");
@@ -22,10 +36,64 @@ export function Dashboard() {
         }}
       >
         <Typography variant="h3" textAlign={"center"}>
-          {isSmall
-            ? "Click a game from the menu icon to start!"
-            : "Click a game on the navbar to start!"}
+          Pick a game to start!
         </Typography>
+        
+        <Box sx={{display: "flex", flexDirection: "row", flexWrap: "wrap", gap: "50px"}}>
+
+          <Card sx={{ maxWidth: 250 }}>
+            <CardActionArea>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: "20px"
+                }}
+              >
+                <AutoStoriesIcon sx={{ width: "100px", height: "100px", color: "rgb(149, 195, 231)" }} />
+              </Box>
+              <CardContent>
+                <Typography gutterBottom variant="h5" textAlign={"center"}>
+                  Memory Dictionary
+                </Typography>
+                <Typography
+                  variant="body2"
+                  textAlign={"center"}
+                  sx={{ color: "text.secondary" }}
+                >
+                  Add as many short term memory words in the dictionary as
+                  possible.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+
+          <Card sx={{ maxWidth: 250 }}>
+            <CardActionArea>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: "20px"
+                }}
+              >
+                <GridViewIcon sx={{ width: "100px", height: "100px", color: "rgb(149, 195, 231)" }} />
+              </Box>
+              <CardContent>
+                <Typography gutterBottom variant="h5" textAlign={"center"}>
+                  Sliding Tiles
+                </Typography>
+                <Typography
+                  variant="body2"
+                  textAlign={"center"}
+                  sx={{ color: "text.secondary" }}
+                >
+                  Move tiles within a set grid to complete the image.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Box>
       </Box>
     </Box>
   );
