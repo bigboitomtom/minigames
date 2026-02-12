d3a889f587071c3662386383a607a94e4330c089
Thomas Guan <thomas@NASA-PC.localdomain>
2026-02-12 18:33:41 +1100

    added cards on dashboard page

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -8,15 +8,29 @@ import {
   useMediaQuery,
 } from "@mui/material";
 import AutoStoriesIcon from "@mui/icons-material/AutoStories";
-import GridViewIcon from '@mui/icons-material/GridView';
+import GridViewIcon from "@mui/icons-material/GridView";
+import { useNavigate } from "react-router-dom";
 
-const games = ["Memory Dictionary", "Sliding Tiles"];
-const links = ["/game/memorydictionary", "/game/slidingtiles"];
-const descriptions = [];
-const icons = [<AutoStoriesIcon />, ]
+const games: string[] = ["Memory Dictionary", "Sliding Tiles"];
+const links: string[] = ["/game/memorydictionary", "/game/slidingtiles"];
+const descriptions: string[] = [
+  "Add as many short term memory words in the dictionary as possible.",
+  "Move tiles within a set grid to complete the image.",
+];
+
+const iconStyle = {
+  width: "100px",
+  height: "100px",
+  color: "rgb(149, 195, 231)",
+};
+const icons = [
+  <AutoStoriesIcon sx={iconStyle} />,
+  <GridViewIcon sx={iconStyle} />,
+];
 
 export function Dashboard() {
-  const isSmall = useMediaQuery("(max-width: 500px)");
+  const navigate = useNavigate();
+
   return (
     <Box
       sx={{
@@ -35,64 +49,47 @@ export function Dashboard() {
           alignItems: "center",
         }}
       >
-        <Typography variant="h3" textAlign={"center"}>
+        <Typography variant="h3" textAlign={"center"} m="40px">
           Pick a game to start!
         </Typography>
-        
-        <Box sx={{display: "flex", flexDirection: "row", flexWrap: "wrap", gap: "50px"}}>
 
-          <Card sx={{ maxWidth: 250 }}>
-            <CardActionArea>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  marginTop: "20px"
-                }}
-              >
-                <AutoStoriesIcon sx={{ width: "100px", height: "100px", color: "rgb(149, 195, 231)" }} />
-              </Box>
-              <CardContent>
-                <Typography gutterBottom variant="h5" textAlign={"center"}>
-                  Memory Dictionary
-                </Typography>
-                <Typography
-                  variant="body2"
-                  textAlign={"center"}
-                  sx={{ color: "text.secondary" }}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            flexWrap: "wrap",
+            gap: "50px",
+          }}
+        >
+          {/* Cards */}
+          {games.map((game, index) => (
+            <Card key={game} sx={{ maxWidth: 250 }}>
+              <CardActionArea onClick={() => navigate(links[index])}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "20px",
+                  }}
                 >
-                  Add as many short term memory words in the dictionary as
-                  possible.
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-
-          <Card sx={{ maxWidth: 250 }}>
-            <CardActionArea>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  marginTop: "20px"
-                }}
-              >
-                <GridViewIcon sx={{ width: "100px", height: "100px", color: "rgb(149, 195, 231)" }} />
-              </Box>
-              <CardContent>
-                <Typography gutterBottom variant="h5" textAlign={"center"}>
-                  Sliding Tiles
-                </Typography>
-                <Typography
-                  variant="body2"
-                  textAlign={"center"}
-                  sx={{ color: "text.secondary" }}
-                >
-                  Move tiles within a set grid to complete the image.
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+                  {icons[index]}
+                </Box>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" textAlign={"center"}>
+                    {game}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    textAlign={"center"}
+                    sx={{ color: "text.secondary" }}
+                  >
+                    {descriptions[index]}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
         </Box>
       </Box>
     </Box>
