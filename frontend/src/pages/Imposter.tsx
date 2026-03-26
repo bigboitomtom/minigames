6dd82128d785731012095086d3bd18b6d7d87bef
Thomas Guan <thomas@NASA-PC.localdomain>
2026-03-26 12:12:58 +1100

    added conditional rendering on game states

--- frontend/src/pages/Imposter.tsx
+++ frontend/src/pages/Imposter.tsx
@@ -4,7 +4,33 @@ import { NumberField } from "../components/NumberField";
 import { useState } from "react";
 
 export function Imposter() {
-  const [numPlayers, setNumPlayers] = useState(3);
+  const [numPlayers, setNumPlayers] = useState<number>(3);
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [isStaging, setIsStaging] = useState<boolean>(false);
+
+  const handleNumPlayers = (value: number) => {
+    setNumPlayers(value);
+    console.log(numPlayers);
+  };
+
+  const handleToStaging = () => {
+    setIsStaging(true);
+  }
+
+  const handleActiveGame = () => {
+    setIsStaging(false);
+    setIsActive(true);
+  }
+
+  const handleNewGame = () => {
+    setIsStaging(true);
+    setIsActive(false);
+  }
+
+  const handleReturnHome = () => {
+    setIsStaging(false);
+    setIsActive(false);
+  }
 
   return (
     <Box
@@ -24,27 +50,54 @@ export function Imposter() {
           alignItems: "center",
         }}
       >
-        <Typography variant="h3" sx={{ marginBottom: "20px" }}>
-          Imposter (Clash Royale Edition)
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "20px",
-          }}
-        >
-          <NumberField label="Number of Players" size="small" min={3} max={8} onChange={(value) => console.log(value)}/>
-          <Button
-            variant="contained"
-            sx={{
-              width: "100%",
-              margin: "0 auto",
-            }}
-          >
-            Start Game
-          </Button>
-        </Box>
+        {!isActive && !isStaging && (
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <Typography variant="h3" sx={{ marginBottom: "20px" }}>
+              Imposter (Clash Royale Edition)
+            </Typography>
+            <Box
+              sx={{
+                width: "30%",
+                display: "flex",
+                flexDirection: "column",
+                margin: "0 auto",
+                gap: "20px",
+              }}
+            >
+              <NumberField
+                label="Number of Players"
+                defaultValue={3}
+                size="small"
+                min={3}
+                max={8}
+                onChange={(value) => handleNumPlayers(value)}
+              />
+              <Button
+                variant="contained"
+                sx={{
+                  width: "100%",
+                  margin: "0 auto",
+                }}
+                onClick={handleToStaging}
+              >
+                Start Game
+              </Button>
+            </Box>
+          </Box>
+        )}
+        {!isActive && isStaging && (
+          <Box sx={{display: "flex", flexDirection: "column" }}>
+            <Typography variant="h1">Staging time</Typography>
+            <Button variant="contained" onClick={handleActiveGame}>Begin</Button>
+          </Box>
+        )}
+        {isActive && !isStaging && (
+          <Box sx={{display: "flex", flexDirection: "column" }}>
+            <Typography variant="h1">Active game time</Typography>
+            <Button variant="contained" onClick={handleNewGame}>New Game</Button>
+            <Button variant="contained" onClick={handleReturnHome}>Back to Home</Button>
+          </Box>
+        )}
       </Box>
     </Box>
   );
